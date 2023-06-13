e6132d31b477f12e1247874f1dfa891cc2c2b5c1
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-13 15:19:08 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name,age) {
+        this.name = name;
+		this.age = age
+    }
+    getName() {
+        return this.name;
+    }
+    setName(newAge) {
+        this.age = newAge;
+        }
+        
+    
+}
+let person = new Person("Jhon")
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(person.getName()+" is studying.")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(person.getName()+" is teaching.")
+	}
+}
+let student = new Student()
+student.study()
+let teacher = new Teacher()
+teacher.teach()
 
 // Do not change the code below this line
 window.Person = Person;
